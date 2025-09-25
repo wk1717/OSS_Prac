9ea3d76a9869b43b6615a896ffdc6c8b16485977
minkyo <smk78449@naver.com>
2025-09-25 15:54:20 +0900

    minkyo 2023111948

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello World!");
+print("Hello World!")
+name="minkyo"
+print(f'I\'m mk Song!')  
+programming_language='Python'  
+print('Most used language is {0}'.format(programming_language))
