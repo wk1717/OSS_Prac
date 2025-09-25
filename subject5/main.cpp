f10f6c50adc70361d9c68dfe6890c5f150684d62
minkyo <smk78449@naver.com>
2025-09-25 17:13:08 +0900

    2025-09-25 개인과제5 4번

--- subject5/main.cpp
+++ subject5/main.cpp
@@ -7,12 +7,17 @@ using namespace std;
 int main(){
     int id;
     string name;
+    string major;
+
     cout << "이름을 입력하세요: ";
     cin >> name;
     cout << "학번을 입력하세요: ";
     cin >> id;
+    cout << "학과를 입력하세요: ";
+    cin >> major;
 
     cout << "\n<출력>\n";
     cout << "이름: " << name;
     cout << "\n학번: " << id;
+    cout << "\n학과: " << major;
 }
