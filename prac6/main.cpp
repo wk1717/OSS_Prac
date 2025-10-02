e8b0761af242d8b53e2ac609428add4103490c8b
minkyo <smk78449@naver.com>
2025-10-02 15:45:16 +0900

    10-02 개인과제6 3번

--- prac6/main.cpp
+++ prac6/main.cpp
@@ -8,6 +8,7 @@ int main(){
     int id;
     string name;
     string major;
+    string university;
 
     cout << "이름을 입력하세요: ";
     cin >> name;
@@ -15,9 +16,12 @@ int main(){
     cin >> id;
     cout << "학과를 입력하세요: ";
     cin >> major;
+    cout << "학교를 입력하세요: ";
+    cin >> university;
 
     cout << "\n<출력>\n";
     cout << "이름: " << name;
     cout << "\n학번: " << id;
     cout << "\n학과: " << major;
+    cout << "\n학교: " << university;
 }
